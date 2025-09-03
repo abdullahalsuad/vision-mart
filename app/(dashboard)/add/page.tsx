ab31999e58dff85952df8895b8b9411777f1b321
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-09-03 23:59:58 +0600

    chore: folder structure setup done

---
+++ app/(dashboard)/add/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>add product</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Vision Mart Tailwind CSS Working</h1>
-    </div>
-  );
-}
